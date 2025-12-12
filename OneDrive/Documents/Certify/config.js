1a0bc0b1c14dfc4e6a03a38fdbca5e979085adca
Akshaya0026 <akshayaarigela0405@gmail.com>
2025-12-12 16:59:51 +0530

    Remove Certify directory from repository

--- OneDrive/Documents/Certify/config.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-  port: process.env.PORT || 3001, // Changed to 3001 to avoid conflict
-  mongoURI: process.env.MONGO_URI,
-  jwtSecret: process.env.JWT_SECRET
-};
